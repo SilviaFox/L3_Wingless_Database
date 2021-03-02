6c4f04c54e1de910dd721eb09a74cd2dd2b89c26
SilviaFox <samonthepeanut@gmail.com>
2021-03-03 10:21:25 +1300

    Updated CSVs and Index

--- index.php
+++ index.php
@@ -8,13 +8,25 @@
     
         <div class="box side paddedside">
             <h2>Hello World!</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis elit ac nibh pulvinar faucibus. Mauris eleifend turpis ut auctor condimentum. Nunc a tortor et quam molestie maximus. Aenean molestie nibh vel euismod rhoncus. Aliquam arcu elit, imperdiet vitae posuere vel, maximus congue risus. Mauris dignissim faucibus sem. Nunc iaculis enim vitae enim ornare gravida. Integer sit amet diam volutpat, gravida enim vitae, vulputate dui.
-
-Donec sagittis laoreet scelerisque. Fusce lacinia eget dolor vitae blandit. Donec id sodales metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam at elit ultrices, eleifend quam bibendum, posuere nisi. Mauris euismod diam vel urna dapibus, eu blandit lacus hendrerit. Aliquam scelerisque molestie justo sit amet facilisis. Praesent tempor hendrerit iaculis. Sed aliquam tempus tortor id scelerisque. Donec justo lectus, interdum et eros eget, porttitor cursus urna. Sed dictum ligula eros, mollis consequat sapien sagittis rhoncus. Ut efficitur, nisl at vehicula pellentesque, turpis tortor lobortis metus, nec luctus arcu justo hendrerit urna. Etiam eget condimentum augue. Vestibulum faucibus odio sed ligula scelerisque pulvinar. Donec dictum commodo mauris at varius.
-
-Duis in semper ipsum, vel sagittis nulla. In ultrices risus eu venenatis tempor. Donec in dignissim dolor. Donec et erat ac risus lobortis feugiat. Aliquam nec diam dui. Etiam at nisi mi. Aliquam erat volutpat. Maecenas hendrerit tristique velit, eu eleifend eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris venenatis ex vel sem tempus, ac lobortis nisi volutpat. Morbi dui orci, bibendum eu posuere quis, faucibus sodales massa. Quisque nibh nunc, congue eget euismod in, finibus ut metus.
-
-Quisque vitae urna ac metus tristique eleifend id a nulla. Donec eu metus nec felis sollicitudin vestibulum. Sed posuere dui sed orci malesuada, quis viverra arcu cursus. Pellentesque tincidunt arcu ac ligula ornare, quis gravida orci dictum. Vestibulum pharetra eu sapien at egestas. Cras eu blandit ex. Vestibulum tincidunt nulla tellus.
+            <p>I can't stop partying, partying
+I can't stop partying, partying
+I gotta have Patrón, I gotta have the beat
+I gotta have a lot of pretty girls around me
+Oh, I can't stop partying, partying
+I can't stop, partying, partying
+I gotta have the cars, I gotta have the jewels
+And if you was me, honey, you would do it too
+Monday to Sunday I hit all the clubs
+And now e'rybody know me when I pull up
+I got the real big posse with me, yeah I'm deep
+And if you lookin' for me I'm in VIP
+Just follow the smoke, they're bringing bottles of the Goose
+And all the girls in the corner getting loose
+Screw rehab I love my addiction
+No sleep, no sleep, I am always on a mission cause
+I can't stop partying, partying
+I can't stop partying, partying
+I gotta have Patrón, I gotta have the
 </p>
         </div> <!-- / side bar -->
         
